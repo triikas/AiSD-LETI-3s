a766e9d1e3cd090fe1e0ef13b0d0c98d098128c5
triikas <triikass@gmail.com>
2023-11-15 13:08:01 +0300

    by-hand

--- lab3.cpp
+++ lab3.cpp
@@ -28,22 +28,88 @@ void FRL_in(Knot* kn) {
 }
 
 //void FRL(Tree* tr) {
+vector<string> Split(const string& str, const char& delimiter) {
+    vector<string> result; // контейнер для хранения подстрок
+
+    stringstream ss(str); // создаем поток из строки
+    string substring; // временная переменная для хранения подстрок
+
+    while (getline(ss, substring, delimiter)) { // пока есть подстроки
+        result.push_back(substring); // добавляем их в контейнер
+    }
+
+    return result; // возвращаем контейнер со всеми подстроками
+}
 //    Knot* kn = tr->get_root();
 //    vector<char> v = {kn->get_let()};
+void GBH(Tree* tr, Knot* kkn) {
+    string inp;
+    vector<string> v;
+    while (inp != "out") {
+        cin >> inp;
+        if (inp == "show") {
+            tr->show("", tr->get_root(), 1);
+        } else {
+            v = Split(inp, '-');
+            if (v[0] == "root" && v.size() > 1) {
+                bool add = false;
+//                Knot* kkn = tr->get_root();
+                for (int i = 1; i < v.size(); ++i) {
+                    if (v[i] == "r" && !add) {
+                        kkn = kkn->get_right();
+                    } else if (v[i] == "c" && !add) {
+                        kkn = kkn->get_center();
+                    } else if (v[i] == "l" && !add) {
+                        kkn = kkn->get_left();
+                    } else if (v[i] == "add" && !add) {
+                        add = true;
+                    } else if (v[i] == "r" && add) {
+                        Knot* kon = new Knot(v[i+1][0]);
+                        kkn->add_kid(kon, 3);
+                        kon->add_father(kkn);
+                        break;
+                    } else if (v[i] == "l" && add) {
+                        Knot* kon = new Knot(v[i+1][0]);
+                        kkn->add_kid(kon, 1);
+                        kon->add_father(kkn);
+                        break;
+                    } else if (v[i] == "c" && add) {
+                        Knot* kon = new Knot(v[i+1][0]);
+                        kkn->add_kid(kon, 2);
+                        kon->add_father(kkn);
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
+
 
 //}
+
+
 
 int main() {
     Tree tr;
 //    tr.create_simple_tree();
-    tr.create_random_tree();
-    tr.show("", tr.get_root(), 1);
-    num_RL = 0;
-    cout << endl << "infix bypass ";
-    auto t1 = chrono::high_resolution_clock::now();
-    FRL_in(tr.get_root());
-    auto t2 = chrono::high_resolution_clock::now();
-    cout << endl << "result:  " << num_RL << endl;
-    cout << "result time: " << chrono::duration_cast<chrono::microseconds>(t2 - t1).count() << " microseconds" << endl;
+    string inp;
+    cout << "do you want to generate tree by hand?" << endl;
+    cin >> inp;
+    if (inp == "y") {
+        GBH(&tr, tr.get_root());
+    } else {
+        tr.create_random_tree();
+        tr.show("", tr.get_root(), 1);
+        num_RL = 0;
+        cout << endl << "infix bypass ";
+        auto t1 = chrono::high_resolution_clock::now();
+        FRL_in(tr.get_root());
+        auto t2 = chrono::high_resolution_clock::now();
+        cout << endl << "result:  " << num_RL << endl;
+        cout << "result time: " << chrono::duration_cast<chrono::microseconds>(t2 - t1).count() << " microseconds" << endl;
+    }
+
     return 0;
 }
